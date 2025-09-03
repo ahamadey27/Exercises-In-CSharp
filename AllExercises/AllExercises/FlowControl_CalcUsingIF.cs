d37c19ab1f7f55748bd5f97f5a345c507eda44ee
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-03 11:16:04 -0400

    all fucedk up
    Add project setup and basic calculator functionality
    
    - Created/modified solution files for Visual Studio 2022.
    - Updated project files to use .NET SDK and target .NET 9.0.
    - Added `FlowControl_CalcUsingIF` class for basic arithmetic operations.
    - Modified `Program.cs` to include a "Hello, World!" message.

---
+++ AllExercises/AllExercises/FlowControl_CalcUsingIF.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllExercises
+{
+    public static class FlowControl_CalcUsingIF
+    {
+         public static void Calculator()
+        {
+            int intA, intB;
+            char operation;
+
+            Console.WriteLine("Enter first number.");
+            intA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter arithmetic.");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Enter second number.");
+            intB = Convert.ToInt32(Console.ReadLine());
+
+            if (operation == '+')
+            {
+                Console.WriteLine($"{intA} {operation} {intB} = {intA + intB}");
+            }
+            else if (operation == '-')
+            {
+                Console.WriteLine($"{intA} {operation} {intB} = {intA - intB}");
+            }
+            else if (operation == '*')
+            {
+                Console.WriteLine($"{intA} {operation} {intB} = {intA * intB}");
+            }
+            else if (operation == '/')
+            {
+                try
+                {
+                    int result = intA / intB;
+                    Console.WriteLine($"{intA} {operation} {intB} = {result}");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Error: Cannot divide by zero " + ex);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation");
+            }
+
+        }
+    }
+}
