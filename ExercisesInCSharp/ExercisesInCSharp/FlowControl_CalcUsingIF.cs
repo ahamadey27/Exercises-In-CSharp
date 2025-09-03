eb721c12b2ebaf500d0c9e30f15615667eed7712
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-03 10:38:30 -0400

    Initialize C# project structure for calculator app
    Created a new Visual Studio solution and project targeting .NET Framework 4.7.2. Added essential references and an application configuration file. Introduced a static class `FlowControl_CalcUsingIF` with a placeholder method for calculator functionality. Set up the `Program` class with a `Main` method as the entry point. Updated `AssemblyInfo.cs` with assembly metadata.

---
+++ ExercisesInCSharp/ExercisesInCSharp/FlowControl_CalcUsingIF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesInCSharp
+{
+    //create a calculator using if statements 
+    public static class FlowControl_CalcUsingIF
+    {
+        public static void Calculator(int num1, char operation, int num2)
+        {
+
+        }
+    }
+}
