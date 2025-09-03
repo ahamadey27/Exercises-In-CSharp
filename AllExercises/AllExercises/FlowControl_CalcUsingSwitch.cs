73b0326768267de29bf8e5ce837c8592c9eec4bb
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-03 11:29:40 -0400

    Add calculator using switch statement and update Program.cs
    Updated `Program.cs` to include the `AllExercises` namespace and call the `Calculator` method. Removed the "Hello, World!" print statement.
    
    Added `FlowControl_CalcUsingSwitch.cs` with a static class that implements a calculator using a switch statement for basic arithmetic operations. Includes user input prompts and error handling for division by zero.

---
+++ AllExercises/AllExercises/FlowControl_CalcUsingSwitch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllExercises
+{
+    //Calculator using switch statement
+    public static class FlowControl_CalcUsingSwitch
+    {
+        public static void Calculator()
+        {
+            int intA, intB;
+            char operation;
+
+            Console.Write("Enter first number: ");
+            intA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter arithmetic: ");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            intB = Convert.ToInt32(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine($"{intA} {operation} {intB} = {intA + intB}");
+                    break;
+                case '-':
+                    Console.WriteLine($"{intA} {operation} {intB} = {intA - intB}");
+                    break;
+
+                case '*':
+                    Console.WriteLine($"{intA} {operation} {intB} = {intA * intB}");
+                    break;
+                case '/':
+                    try
+                    {
+                        decimal resultDivide = intA / intB;
+                        Console.WriteLine($"{intA} {operation} {intB} = {intA / intB}");
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        Console.WriteLine("Error: Cannot divide by zero: " + ex);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
+        }
+    }
+}
