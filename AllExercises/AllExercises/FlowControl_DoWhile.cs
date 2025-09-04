aa201fc1aebe9c343e0aece0d4c59105604c049d
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-04 12:08:17 -0400

    Add FlowControl_DoWhile class for user input processing
    Introduces a new C# class `FlowControl_DoWhile` in the `AllExercises` namespace.
    The class features a static method `DoWhile()` that repeatedly prompts the user for a number,
    multiplies it by 10, and displays the result until the user enters 0.
    Includes necessary using directives for system libraries.

---
+++ AllExercises/AllExercises/FlowControl_DoWhile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllExercises
+{
+    //Create a program in C# that requests a number (x) and displays 10*x. Must be repeated until user enters 0.
+    public static class FlowControl_DoWhile
+    {
+        public static void DoWhile()
+        {
+            int x = 0;
+
+            do
+            {
+                Console.WriteLine("Enter a number");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(x * 10);
+            }
+
+            while (x != 0);
+        }
+
+    }
+}
