7aaf2eccd2f3a2478d0fdce4354e9b7d6cebeceb
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-10 17:15:22 -0400

    Add FlowControlA_Mathmatical_Statistics class
    Introduces a new C# class for calculating mathematical statistics
    from an array of integers, including sum, mean, min, and max.
    The implementation is incomplete as it does not return or display
    the calculated statistics.

---
+++ AllExercises/AllExercises/FlowControlA_Mathmatical_Statistics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllExercises
+{
+    /* 
+      Create a C# program that prompts the user for 5 integers and displays the following mathematical statistics on the screen:
+        The sum of the 5 numbers
+        The arithmetic mean
+        The maximum number
+        The minimum number
+    */
+    public static class FlowControlA_Mathmatical_Statistics
+    {
+        public static void StatsReturn(int[] numbers)
+        {
+            int total = numbers.Length;
+
+            int sum = 0;
+
+            foreach (int i in numbers)
+            {
+                sum += i;
+            }
+
+            decimal average = sum / total; 
+            int min = numbers.Min();
+            int max = numbers.Max();
+            
+
+
+        }
+
+
+    }
+}
