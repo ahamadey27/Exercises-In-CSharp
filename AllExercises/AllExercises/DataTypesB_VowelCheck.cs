0c01280040be028a8be4d3e8615bba5e9c8fffe1
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-17 10:47:28 -0400

    Refactor VowelCheck method to improve input handling
    The VowelCheck method was updated to return void instead of a string.
    A user prompt was added to request a letter, and input validation
    was implemented using a while loop with char.TryParse. The input
    is converted to lowercase for case-insensitive comparison. A switch
    statement was introduced to determine if the input is a vowel,
    including 'y' as a vowel. Informative messages are displayed based
    on the input.

--- AllExercises/AllExercises/DataTypesB_VowelCheck.cs
+++ AllExercises/AllExercises/DataTypesB_VowelCheck.cs
@@ -9,8 +9,39 @@ namespace AllExercises
     //Create a C# program that asks the user for a letter (x) and check if it is a vowel or other symbol.
     public static class DataTypesB_VowelCheck
     {
-        public static string VowelCheck()
+        public static void VowelCheck()
         {
+            Console.WriteLine("Enter a letter to check if its a vowel.");
+
+            char word;
+
+            while (true)
+            {
+                if (!char.TryParse(Console.ReadLine(), out word))
+                {
+                    Console.WriteLine("Enter a valid character");
+                    continue;
+                }
+
+                break;
+            }
+
+            word = char.ToLower(word);
+
+            switch(word)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'y':
+                    Console.WriteLine($"{word} is a vowel");
+                    break;
+                default:
+                    Console.WriteLine($"{word} is not a vowel");
+                    break;
+            }
 
         }
     }
