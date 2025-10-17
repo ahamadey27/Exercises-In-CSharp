82c3603b037e9177dec7493541ad78645675e24d
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-17 10:58:56 -0400

    Add DataTypesB_CheckIfNumber class with placeholder method
    Introduced the `DataTypesB_CheckIfNumber` static class within the
    `AllExercises` namespace. Added a `NumberCheck` method as a
    placeholder for functionality to check if a text input is a number.
    
    Included necessary `using` directives and a comment describing
    the purpose of the program.

---
+++ AllExercises/AllExercises/DataTypesB_CheckIfNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllExercises
+{
+    //Create a C# program that asks the user for a text (x) and converts to check if the text is a number
+    public static class DataTypesB_CheckIfNumber
+    {
+        public static void NumberCheck()
+        {
+
+        }
+    }
+}
